c00e3a530204e57cd339092c183fec672e246ad7
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2021-06-28 00:44:40 +0600

    shopping module added

--- Course Project/Foodies/src/app/app.module.ts
+++ Course Project/Foodies/src/app/app.module.ts
@@ -4,8 +4,6 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {DropdownDirective} from './shared/dropdown.directive';
 import {AppRoutingModule} from './app-routing.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -15,14 +13,13 @@ import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import { AlertComponent } from './shared/alert/alert.component';
 import {RecipesModule} from "./recipes/recipes.module";
 import {RecipesRoutingModule} from "./recipes/recipes-routing.module";
+import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
@@ -36,6 +33,7 @@ import {RecipesRoutingModule} from "./recipes/recipes-routing.module";
     AppRoutingModule,
     RecipesModule,
     RecipesRoutingModule,
+    ShoppingListModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
