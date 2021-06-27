8fe3625fa034576442f9684925c43c4281bbbab7
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2021-06-28 01:58:54 +0600

    lazy load added for shopping list and auth

--- Course Project/Foodies/src/app/app.module.ts
+++ Course Project/Foodies/src/app/app.module.ts
@@ -6,10 +6,7 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
-import {AuthModule} from "./auth/auth.module";
-
 
 @NgModule({
   declarations: [
@@ -20,14 +17,11 @@ import {AuthModule} from "./auth/auth.module";
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ShoppingListModule,
     SharedModule,
-    AuthModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
