6c05df9cdf5fa5fa2af9e9d48480f421459a0981
Abir Hasan Mubin <abirhasanmubin@gmail.com>
2021-06-28 01:52:47 +0600

    lazy load added for recipes

--- Course Project/Foodies/src/app/app.module.ts
+++ Course Project/Foodies/src/app/app.module.ts
@@ -6,8 +6,6 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import {RecipesModule} from "./recipes/recipes.module";
-import {RecipesRoutingModule} from "./recipes/recipes-routing.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
 import {AuthModule} from "./auth/auth.module";
@@ -22,8 +20,6 @@ import {AuthModule} from "./auth/auth.module";
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
-    RecipesRoutingModule,
     ShoppingListModule,
     SharedModule,
     AuthModule
